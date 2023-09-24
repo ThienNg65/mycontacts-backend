1a47f8ab0a07697da797753bb3f55328b563a8ad
Thien Nguyen <nvtt1510@gmail.com>
2023-09-24 19:38:08 +0700

    user register and hash password

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -4,7 +4,7 @@ const connecDb = async () => {
     try {
         const connect = await mongoose.connect(process.env.CONNECTION_STRING);
         console.log("Database connected! ",
-            connect.connection.host, 
+            connect.connection.host,
             connect.connection.name
         );
     } catch (err) {
